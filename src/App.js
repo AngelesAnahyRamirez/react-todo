f0cee3240c2d824df999366556161d15ba93aee6
AngelesAnahyRamirez <angelesanahir4@gmail.com>
2019-08-22 21:48:04 -0500

    develop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import { typography } from '@material-ui/system';
+import { Grid, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+const App = () => {
+  return {
+  <React.fragment>
+    <Typography variant="h2" align="center" gutterBottom>
+      To-do list 
+  </Typography>
+      <Grid container justify="center">
+        <Grid item>
+         <TextField type="text" placeholder=" Add todo ...." margin="normal" />
+        </Grid>
+      </Grid>
+      <Grid container justify="center">
+      <Grid item md={8}>
+        <List>
+          <ListItem button>
+            <Checkbox />
+            <ListItemText primary="checar el prework " />
+            <ListItemSecondaryAction>
+            <IconButton>
+            <DeleteIcon/>
+            </ IconButton>
+            </ListItemSecondaryAction>
+
+  </ListItem>
+  </List>
+  </Grid>
+  </Grid>
+  <React.fragment>
+  
+
+
+          }
+      }
+      
+      export default App;
